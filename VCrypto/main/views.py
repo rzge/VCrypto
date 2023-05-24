38d31d3d4cd06591d85c2b5182a43a28a62853d2
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-05-24 20:33:49 +0300

    добавил обработку ошибок 404

--- VCrypto/main/views.py
+++ VCrypto/main/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse('success')
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Не найдено(</h1>')
