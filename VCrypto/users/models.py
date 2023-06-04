514b1ac27b1d850dbbb51ab037a6cc2cd1fce676
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-06-04 15:47:43 +0300

    Создана кастомная модель пользователя (для дальнейшего добавления полей друзей, адреса биткоин-кошелька и фотографии)

--- VCrypto/users/models.py
+++ VCrypto/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+
+    def __str__(self):
+        return self.username
+
+# Create your models here.
+
 
 # Create your models here.
