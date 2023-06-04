8bb5c0c1bf82a7d88899d8bbc85e820db72b0978
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-06-04 23:14:30 +0300

    маршрут и функция для поисковой строки

--- VCrypto/main/urls.py
+++ VCrypto/main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('search_results/', views.search_results, name='search_results'),
 ]
--- VCrypto/main/views.py
+++ VCrypto/main/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render, redirect
 from users.forms import UserRegisterForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from users import models
 
 def index(request):
     return render(request, 'main/index.html')
+
 
 def register(request):
     if request.method == 'POST':
@@ -22,7 +24,17 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'main/about_you.html') #возможность видеть личную информацию
+    return render(request, 'main/about_you.html')  # возможность видеть личную информацию
+
+
+def search_results(request):  # поисковая строка
+    query = request.GET.get('query')
+    if not query:
+        query = ""
+    results = models.CustomUser.objects.filter(username__icontains=query)
+    print(results)
+    return render(request, 'main/search_results.html', {'results': results, 'query': query})
+
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Не найдено(</h1>')
