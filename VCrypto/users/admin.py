cc86fb6bb906cd17ce6a12807af66907eb1a2ca4
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-06-04 15:44:46 +0300

    Позволяет смотреть и регистрировать наших пользователей через админку

--- VCrypto/users/admin.py
+++ VCrypto/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .forms import UserRegisterForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserRegisterForm
+    model = CustomUser
+    list_display = ['email', 'username',]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 # Register your models here.
