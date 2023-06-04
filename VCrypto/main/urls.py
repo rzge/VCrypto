b19d506b3adb35c25230b291415e3da427e4a5ed
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-06-04 15:51:11 +0300

    добавлены маршруты и их функции представления (для регистрации и отображения инфы о профиле)

--- VCrypto/main/urls.py
+++ VCrypto/main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
--- VCrypto/main/views.py
+++ VCrypto/main/views.py
@@ -1,9 +1,28 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from users.forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'main/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Создан аккаунт {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'main/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'main/about_you.html') #возможность видеть личную информацию
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Не найдено(</h1>')
