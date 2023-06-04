0e9c906918e36a4844ef050d8012afa29c1c2101
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-06-04 15:49:31 +0300

    зарегистрировал новые приложения + добавил стили для криспи форм + редирект после регистрации + кастомная модель для аутентификации

--- VCrypto/VCrypto/settings.py
+++ VCrypto/VCrypto/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig'
+    'main.apps.MainConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,12 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+LOGIN_REDIRECT_URL = 'profile' #main
+LOGIN_URL = 'login'
+
+AUTH_USER_MODEL = 'users.CustomUser'
