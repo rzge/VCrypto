a68f72c7944b4a833c424e787897e314500cf037
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-06-04 15:45:10 +0300

    добавлена форма регистрации пользователей

---
+++ VCrypto/users/forms.py
@@ -0,0 +1,8 @@
+from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password1', 'password2']
