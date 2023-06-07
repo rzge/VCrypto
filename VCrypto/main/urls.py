637e969750e5500640d998516a001297ef9ba7be
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-06-07 19:16:07 +0300

    добавлена возможность отменять запрос дружбы + изменил текст ответа сайта + добавлен url маршрут для добавления пользователя в друзья

--- VCrypto/main/urls.py
+++ VCrypto/main/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('search_results/', views.search_results, name='search_results'),
-    path('search_results/send_friend_request/<int:userID>', views.send_friend_request, name='send_friend_request')
+    path('search_results/send_friend_request/<int:userID>', views.send_friend_request, name='send_friend_request'),
+    path('search_results/cancel_friend_request/<int:userID>', views.cancel_friend_request, name='cancel_friend_request')
 ]
--- VCrypto/main/views.py
+++ VCrypto/main/views.py
@@ -41,9 +41,33 @@ def send_friend_request(request, userID):
     receiver = models.CustomUser.objects.get(id=userID)
     friend_request, created = models.FriendRequest.objects.get_or_create(sender=sender, receiver=receiver)
     if created:
-        return HttpResponse('friend request sent')
+        return HttpResponse('Отправлен запрос дружбы')
     else:
-        return HttpResponse('friend request was already sent')
+        return HttpResponse('Запрос дружбы уже был отправлен')
+
+@login_required
+def cancel_friend_request(request, userID): #отменяет отправленный запрос дружбы
+    sender = request.user
+    receiver = models.CustomUser.objects.get(id=userID)
+    print(sender)
+    print(receiver)
+    if models.FriendRequest.objects.filter(sender=sender, receiver=receiver).delete()[0] != 0:
+        return HttpResponse('Запрос дружбы отменён')
+    else:
+        return HttpResponse('Запрос дружбы уже был отменён')
+
+    #
+    # return redirect('profile', to_user_id)
+
+    # unfriended_user = models.CustomUser.objects.get(id=userID)
+    # friend_request = models.FriendRequest.objects.get(receiver=unfriended_user)
+    # print(friend_request.receiver.id)
+    # #friend_request.delete()
+    # if request.user == friend_request.from_user:
+    #     friend_request.delete()
+    #     return redirect('user_profile', user_id=friend_request.to_user.id)
+    # else:
+    #     return redirect('home')
 
 def pageNotFound(request, exception):
     return render(request, 'main/404.html')
