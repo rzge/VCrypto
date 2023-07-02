a4f13cc05345bef6e20a8cf6db96322d6e0431ae
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-07-02 23:30:25 +0300

    добавил темплейт тэг, чтобы отображать айди отправителей

--- VCrypto/main/templatetags/extras.py
+++ VCrypto/main/templatetags/extras.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.simple_tag()
 def get_friend_requests_receiver():
     return [x['receiver'] for x in FriendRequest.objects.values('receiver')]
+
+@register.simple_tag()
+def get_friend_requests_sender():
+    return [x['sender'] for x in FriendRequest.objects.values('sender')]
