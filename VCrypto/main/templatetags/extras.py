5b7e2c93206078b9dedb189675b5059a890ad93f
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-07-05 13:00:29 +0300

    добавил кастомный тег для отображения получателей запроса дружбы от зарегистрированного пользователя + исправил баг по отображению отправленного запроса дружбы

--- VCrypto/main/templatetags/extras.py
+++ VCrypto/main/templatetags/extras.py
@@ -1,12 +1,20 @@
 from django import template
 from users.models import *
+
 register = template.Library()
 
 
 @register.simple_tag()
-def get_friend_requests_receiver():
+def get_friend_requests_receiver():  # показывает всех получателей
     return [x['receiver'] for x in FriendRequest.objects.values('receiver')]
 
+
 @register.simple_tag()
-def get_friend_requests_sender():
+def get_friend_requests_sender():  # показывает всех отправителей
     return [x['sender'] for x in FriendRequest.objects.values('sender')]
+
+
+@register.simple_tag(name='receivers')
+def receivers_list(user_id=None):  # показывает получателей от конкретного юзера
+    receiver_ids = FriendRequest.objects.filter(sender=user_id).values_list('receiver', flat=True)
+    return receiver_ids
