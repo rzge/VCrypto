010611451d67a47028ded19365fa93e434c64d1b
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-07-06 18:27:54 +0300

    добавил функцию для отображения друзей

--- VCrypto/main/urls.py
+++ VCrypto/main/urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('search_results/', views.search_results, name='search_results'),
+    path('friends/', views.friends, name='friends'),
     path('search_results/send_friend_request/<int:userID>', views.send_friend_request, name='send_friend_request'),
     path('search_results/cancel_friend_request/<int:userID>', views.cancel_friend_request, name='cancel_friend_request'),
     path('search_results/decline_friend_request/<int:userID>', views.decline_friend_request, name='decline_friend_request'),
     path('search_results/accept_friend_request/<int:userID>', views.accept_friend_request, name='accept_friend_request'),
-    path('search_results/unfriend/<int:userID>', views.unfriend, name='unfriend')
+    path('search_results/unfriend/<int:userID>', views.unfriend, name='unfriend'),
 ]
